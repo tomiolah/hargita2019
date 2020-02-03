975e6550f9f332a178e55f0900e64e6fe12f401f
Tomi Olah <tomiolah1998@gmail.com>
2020-02-03 16:47:48 +0200

    Beautify Python code

--- code/xml2plaintext/main.py
+++ code/xml2plaintext/main.py
@@ -1,12 +1,15 @@
 import re, os
 from models.XMLFile import XMLFile
 
-files = [ f 
-  for f in [
-      os.path.join('..', '..', 'OpenLP Database', 'Songs', x)
-      for x in os.listdir('../../OpenLP Database/Songs')
-      if re.match(r'.*xml', x)
-    ]
+files = [
+  f 
+  for f
+  in [
+    os.path.join('..', '..', 'OpenLP Database', 'Songs', x)
+    for x
+    in os.listdir('../../OpenLP Database/Songs')
+    if re.match(r'.*xml', x)
+  ]
   if os.path.isfile(f)
 ]
 
