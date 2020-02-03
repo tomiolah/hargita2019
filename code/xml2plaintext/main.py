a7ccc552e510a7ff3601026c81e19dc06441c4ef
Tomi Olah <tomiolah1998@gmail.com>
2020-02-03 16:44:09 +0200

    Use higher level functions for getting XML filenames
    Maybe this can make things more OS-agnostic

--- code/xml2plaintext/main.py
+++ code/xml2plaintext/main.py
@@ -1,9 +1,15 @@
-from os import popen
-import re
+import re, os
 from models.XMLFile import XMLFile
 
-with popen('find ../../OpenLP\\ Database/Songs') as ls:
-  files = [ _.strip() for _ in ls.readlines()[1:] if re.match(r'.*xml', _)  ]
+files = [ f 
+  for f in [
+      os.path.join('..', '..', 'OpenLP Database', 'Songs', x)
+      for x in os.listdir('../../OpenLP Database/Songs')
+      if re.match(r'.*xml', x)
+    ]
+  if os.path.isfile(f)
+]
+
 xmls = [ XMLFile(f) for f in files ]
 for x in xmls:
   x.write_plaintext()
